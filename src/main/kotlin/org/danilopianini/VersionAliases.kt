de4f406c3fa23a685db17f9374ed50962d42296a
Danilo Pianini <danilo.pianini@unibo.it>
2020-12-24 12:18:42 +0100

    Document the public properties

--- src/main/kotlin/org/danilopianini/VersionAliases.kt
+++ src/main/kotlin/org/danilopianini/VersionAliases.kt
@@ -2,7 +2,14 @@ package org.danilopianini
 
 import java.lang.IllegalStateException
 
+/**
+ * Utility methods collection, to be exposed to settings.gradle.kts.
+ */
 object VersionAliases {
+
+    /**
+     * The aliases of refreshversions-aliases.
+     */
     val additionalAliases: String = Thread
         .currentThread()
         .contextClassLoader
@@ -10,5 +17,9 @@ object VersionAliases {
         ?.readText()
         ?: throw IllegalStateException("Unable to find the version aliases")
 
+    /**
+     * A single-element list with just the aliases of refreshversions-aliases.
+     * Shortcut to simply inject these aliases and nothing else.
+     */
     val justAdditionalAliases: List<String> = listOf(additionalAliases)
 }
